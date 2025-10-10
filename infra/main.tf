563e2b2879c726fef3ad542e10d251df9e892db4
Urquilla <E114983@mcgladrey.rsm.net>
2025-10-09 20:15:14 -0600

    Adding terraform provider to get connected to Kubernetes cluster

--- infra/main.tf
+++ infra/main.tf
@@ -22,6 +22,15 @@ provider "google-beta" {
   region  = var.region
 }
 
+
+provider "kubernetes" {
+  host                   = google_container_cluster.primary.endpoint
+  cluster_ca_certificate = base64decode(google_container_cluster.primary.master_auth[0].cluster_ca_certificate)
+  token                  = data.google_client_config.default.access_token
+}
+
+data "google_client_config" "default" {}
+
 # GKE 
 
 resource "google_container_cluster" "primary" {
