eb3f04254143630b38bc391e40c04c84d1aa3dd8
Urquilla <E114983@mcgladrey.rsm.net>
2025-10-16 21:15:43 -0600

    Integrating helm to project

--- Dockerfile
+++ Dockerfile
@@ -1,16 +1,20 @@
-FROM node:18-alpine
+FROM node:18-alpine AS builder
 WORKDIR /app
 COPY package*.json ./
 RUN npm install
 COPY . .
-ENV NODE_ENV=production
-EXPOSE 8000
 
+FROM node:18-alpine
+WORKDIR /app
+
+COPY --from=builder /app /app
 
 RUN mkdir -p /app/data && \
     chown -R node:node /app/data
 
+ENV NODE_ENV=production
 ENV DATABASE_NAME=/app/data/dev.sqlite
+EXPOSE 8000
 
 HEALTHCHECK CMD curl --fail http://localhost:8000/api/users || exit 1
 CMD ["node", "index.js"]
--- infra/outputs.tf
+++ infra/outputs.tf
@@ -13,3 +13,23 @@ output "docker_repo_url" {
 output "load_balancer_ip" {
   value = kubernetes_service.node_app_lb.status[0].load_balancer[0].ingress[0].ip
 }
+
+output "dns_zone_name" {
+  value       = google_dns_managed_zone.urquilladev.name
+  description = "Nombre de la zona DNS gestionada para urquilladev.xyz"
+}
+
+output "dns_zone_nameservers" {
+  value       = google_dns_managed_zone.urquilladev.name_servers
+  description = "Cloud DNS Nameservers"
+}
+
+output "ssl_certificate_name" {
+  value       = google_compute_managed_ssl_certificate.urquilladev_cert.name
+  description = "Nombre del certificado SSL gestionado por GCP"
+}
+
+output "dns_record_ip" {
+  value       = google_dns_record_set.urquilladev_a.rrdatas[0]
+  description = "IP pública del Load Balancer"
+}
