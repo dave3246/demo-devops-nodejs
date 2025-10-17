750c63d3614495f8dbdb8bcdd30cc70f50903ce2
Urquilla <E114983@mcgladrey.rsm.net>
2025-10-16 23:00:46 -0600

    Applying changes to main tf to enable autoescale

--- infra/main.tf
+++ infra/main.tf
@@ -22,21 +22,52 @@ provider "google-beta" {
   region  = var.region
 }
 
+
+provider "kubernetes" {
+  host                   = google_container_cluster.primary.endpoint
+  cluster_ca_certificate = base64decode(google_container_cluster.primary.master_auth[0].cluster_ca_certificate)
+  token                  = data.google_client_config.default.access_token
+}
+
+data "google_client_config" "default" {}
+
 # GKE 
 
 resource "google_container_cluster" "primary" {
   name     = "devops-cluster"
   location = var.zone
 
+  remove_default_node_pool = true
+
   initial_node_count = 2
+
+  master_auth {
+    client_certificate_config {
+      issue_client_certificate = false
+    }
+  }
+}
+
+resource "google_container_node_pool" "default_pool" {
+  name       = "default-pool"
+  location   = var.zone
+  cluster    = google_container_cluster.primary.name
+  node_count = 2
+
+  autoscaling {
+    min_node_count = 2
+    max_node_count = 5
+  }
 
   node_config {
     machine_type = "e2-medium"
   }
 
-  enable_autopilot = false
+  management {
+    auto_upgrade = true
+    auto_repair  = true
+  }
 }
-
 
 # Artifact Registry 
 
@@ -47,3 +78,22 @@ resource "google_artifact_registry_repository" "docker_repo" {
   format        = "DOCKER"
 }
 
+resource "kubernetes_service" "node_app_lb" {
+  metadata {
+    name      = "node-app-lb"
+    namespace = "default"
+  }
+
+  spec {
+    selector = {
+      app = "node-app"
+    }
+
+    port {
+      port        = 80
+      target_port = 8000
+    }
+
+    type = "LoadBalancer"
+  }
+}
